80248e74f54e070f0b09a8b5c918025bf2788adf
Teejay Dixon <tahjgdixon@gmail.com>
2022-07-25 21:07:24 -0700

    commit tree 8dadccfb9ea02d99d6e4012862ae4bb8a8a7779a

---
+++ index.js
@@ -0,0 +1,55 @@
+function add() {
+
+}
+
+function subtract() {
+
+}
+
+function multiply() {
+
+}
+
+function divide() {
+
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function increment(n) {
+  return ++n;
+}
+
+function decrement(n) {
+  return --n;
+}
+
+
+function makeInt(n) {
+
+  return parseInt(n, 10 || "2.32567");
+
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n, "2.3456667");
+}
+
+
+
+
+
